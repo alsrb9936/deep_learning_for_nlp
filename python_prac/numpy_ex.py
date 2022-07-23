e14403b28157492b9309b98a4fc97c077ac919ca
Mingyu <ghfn123@gmail.com>
2022-07-23 19:53:54 +0900

    update numpy python prac

--- python_prac/numpy_ex.py
+++ python_prac/numpy_ex.py
@@ -40,5 +40,42 @@ print("\n",random_mat)
 # 0 ~ n-1 을 가지는 배열
 range_vec = np.arange(4)
 print("\n",range_vec)
-# 다른 바리에이션 i 부터 j 까지 k 씩 증가 (i,j,k)
 
+# 다른 바리에이션 i 부터 j 까지 k 씩 증가 (i,j,k)
+n = 2
+range_n_vec = np.arange(1,10,n)
+print("\n", range_n_vec)
+
+# 배열 구조만 바꾸기 reshape 
+reshape_map = np.arange(30).reshape(5,6)
+print('\n',reshape_map)
+
+# numpy 슬라이싱 기능
+mat = np.array([[1,2,3],[4,5,6]])
+print('\n',mat)
+# 첫번째 행 출력
+slice_mat = mat[0, :]
+print('\n',slice_mat)
+# 두번째 열 출력
+slice_mat = mat[:,1]
+print(slice_mat)
+
+# numpy 정수 인덱싱
+mat = np.array([[1, 2], [4, 5], [7, 8]])
+print(mat)
+# 1행 2열, 3행 1열 원소 모아서 새 배열 만들기 [1행 , 3행], [2열, 1열]
+indexing_mat = mat[[0,2],[1,0]]
+print('\n',indexing_mat)
+
+# numpy 연산
+# 원소 합과 곱, 나누기, 빼기
+x = np.array([1,2,3])
+y = np.array([4,5,6])
+print('\n',np.add(x,y))
+print(np.multiply(x,y))
+print(np.divide(x,y))
+print(np.subtract(x,y))
+#행렬곱,벡터곱
+mat = np.array([[1,2],[3,4]])
+mat2 = np.array([[5,6],[7,8]])
+print('\n',np.dot(mat,mat2))
